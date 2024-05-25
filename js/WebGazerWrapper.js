220e21df970a4e5be5e74a04325865a843043e8f
Hakar Dar <hdar@htwsaar.de>
2024-05-25 20:53:18 +0200

    tests for screen gaze direction boundaries

--- js/WebGazerWrapper.js
+++ js/WebGazerWrapper.js
@@ -2,7 +2,28 @@ webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
         return;
     }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(elapsedTime); //elapsed time is based on time since begin was called
+    var xPrediction = data.x; //these x coordinates are relative to the viewport
+    var yPrediction = data.y;
+    var screenHeight = window.innerHeight;
+    var screenWidth = window.innerWidth;//these y coordinates are relative to the viewport
+
+    var topBoundary = screenHeight -(screenWidth);
+    //Events for up and down
+    if(yPrediction < screenHeight * 0.5) {
+        console.log("Du schaust nach oben")
+    }
+    if(yPrediction > screenHeight * 0.5) {
+        console.log("Du schaust nach unten")
+    }
+    //Events for left and right
+    if(xPrediction < screenWidth * 0.25) {
+        console.log("Du schaust nach links")
+    }
+    if(yPrediction > screenWidth * 0.25) {
+        console.log("Du schaust nach rechts")
+    }
 }).begin();
+
+webgazer.showPredictionPoints(true);
+
+webgazer.begin();
