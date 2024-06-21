8dd16520ce6b3b7abd1de16b27731ae9c69fd927
Leonard <neagoeleonard21@gmail.com>
2024-06-21 17:48:41 +0200

    fixes

--- js/webSpeechContent.js
+++ js/webSpeechContent.js
@@ -1,12 +1,4 @@
 // Inject WebSpeechWrapper.js into the webpage
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('js/WebSpeechWrapper.js');
-script.onload = function() {
-    this.remove();
-    // Initialize the WebSpeechWrapper and start it
-    const speechWrapper = new WebSpeechWrapper();
-    speechWrapper.start();
-};
-
-
+const script = document.createElement("script");
+script.src = chrome.runtime.getURL("js/WebSpeechWrapper.js");
 (document.head || document.documentElement).appendChild(script);
