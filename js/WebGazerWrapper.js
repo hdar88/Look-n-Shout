196a4fbc96051a6118dbe7b153d5148d7b940f21
Leonard <neagoeleonard21@gmail.com>
2024-06-28 23:25:55 +0200

    dropdown-design

--- js/WebGazerWrapper.js
+++ js/WebGazerWrapper.js
@@ -1,91 +1,91 @@
 webgazer
-    .setGazeListener(function (data) {
-        if (data == null) {
-            return;
-        }
-        let xPrediction = data.x;
-        let yPrediction = data.y;
+  .setGazeListener(function (data) {
+    if (data == null) {
+      return;
+    }
+    let xPrediction = data.x;
+    let yPrediction = data.y;
 
-        let topBoundary = document.getElementById("topBoundary");
-        let bottomBoundary = document.getElementById("bottomBoundary");
-        let leftBoundary = document.getElementById("leftBoundary");
-        let rightBoundary = document.getElementById("rightBoundary");
+    let topBoundary = document.getElementById("topBoundary");
+    let bottomBoundary = document.getElementById("bottomBoundary");
+    let leftBoundary = document.getElementById("leftBoundary");
+    let rightBoundary = document.getElementById("rightBoundary");
 
-        // define position and size of boundaries
-        let topRect = topBoundary.getBoundingClientRect();
-        let bottomRect = bottomBoundary.getBoundingClientRect();
-        let leftRect = leftBoundary.getBoundingClientRect();
-        let rightRect = rightBoundary.getBoundingClientRect();
+    // define position and size of boundaries
+    let topRect = topBoundary.getBoundingClientRect();
+    let bottomRect = bottomBoundary.getBoundingClientRect();
+    let leftRect = leftBoundary.getBoundingClientRect();
+    let rightRect = rightBoundary.getBoundingClientRect();
 
-        // check in which boundary gaze is positioned
-        if (
-            yPrediction >= topRect.top &&
-            yPrediction <= topRect.bottom &&
-            xPrediction >= topRect.left &&
-            xPrediction <= topRect.right
-        ) {
-            //console.log("Du schaust nach oben");
-            simulateKeyPress("ArrowUp");
-        } else if (
-            yPrediction >= bottomRect.top &&
-            yPrediction <= bottomRect.bottom &&
-            xPrediction >= bottomRect.left &&
-            xPrediction <= bottomRect.right
-        ) {
-            simulateKeyPress("ArrowDown");
-        } else if (
-            yPrediction >= leftRect.top &&
-            yPrediction <= leftRect.bottom &&
-            xPrediction >= leftRect.left &&
-            xPrediction <= leftRect.right
-        ) {
-            simulateKeyPress("ArrowLeft");
-        } else if (
-            yPrediction >= rightRect.top &&
-            yPrediction <= rightRect.bottom &&
-            xPrediction >= rightRect.left &&
-            xPrediction <= rightRect.right
-        ) {
-            simulateKeyPress("ArrowRight");
-        }
-    })
-    .setTracker("TFFacemesh")
-    .begin()
+    // check in which boundary gaze is positioned
+    if (
+      yPrediction >= topRect.top &&
+      yPrediction <= topRect.bottom &&
+      xPrediction >= topRect.left &&
+      xPrediction <= topRect.right
+    ) {
+      //console.log("Du schaust nach oben");
+      simulateKeyPress("ArrowUp");
+    } else if (
+      yPrediction >= bottomRect.top &&
+      yPrediction <= bottomRect.bottom &&
+      xPrediction >= bottomRect.left &&
+      xPrediction <= bottomRect.right
+    ) {
+      simulateKeyPress("ArrowDown");
+    } else if (
+      yPrediction >= leftRect.top &&
+      yPrediction <= leftRect.bottom &&
+      xPrediction >= leftRect.left &&
+      xPrediction <= leftRect.right
+    ) {
+      simulateKeyPress("ArrowLeft");
+    } else if (
+      yPrediction >= rightRect.top &&
+      yPrediction <= rightRect.bottom &&
+      xPrediction >= rightRect.left &&
+      xPrediction <= rightRect.right
+    ) {
+      simulateKeyPress("ArrowRight");
+    }
+  })
+  .setTracker("TFFacemesh")
+  .begin();
 webgazer.clearData();
 
 async function simulateKeyPress(key) {
-    const keyCodeMap = {
-        w: 87,
-        a: 65,
-        s: 83,
-        d: 68,
-        " ": 32,
-        ArrowUp: 38,
-        ArrowDown: 40,
-        ArrowLeft: 37,
-        ArrowRight: 39,
-    };
+  const keyCodeMap = {
+    w: 87,
+    a: 65,
+    s: 83,
+    d: 68,
+    " ": 32,
+    ArrowUp: 38,
+    ArrowDown: 40,
+    ArrowLeft: 37,
+    ArrowRight: 39,
+  };
 
-    const keyCode = keyCodeMap[key];
-    if (!keyCode) return;
+  const keyCode = keyCodeMap[key];
+  if (!keyCode) return;
 
-    const eventInit = {
-        key: key,
-        code: key.toUpperCase(),
-        keyCode: keyCode,
-        which: keyCode,
-        bubbles: true,
-        cancelable: true,
-    };
+  const eventInit = {
+    key: key,
+    code: key.toUpperCase(),
+    keyCode: keyCode,
+    which: keyCode,
+    bubbles: true,
+    cancelable: true,
+  };
 
-    const eventDown = new KeyboardEvent("keydown", eventInit);
-    const eventUp = new KeyboardEvent("keyup", eventInit);
+  const eventDown = new KeyboardEvent("keydown", eventInit);
+  const eventUp = new KeyboardEvent("keyup", eventInit);
 
-    document.dispatchEvent(eventDown);
-    await new Promise((r) => setTimeout(r, 500));
-    document.dispatchEvent(eventUp);
+  document.dispatchEvent(eventDown);
+  await new Promise((r) => setTimeout(r, 500));
+  document.dispatchEvent(eventUp);
 }
 
 function simulateKeyCombiPress(keys) {
-    //TODO
+  //TODO
 }
--- js/popup.js
+++ js/popup.js
@@ -13,6 +13,10 @@ const arrowUp = document.getElementById("up-voice");
 const arrowDown = document.getElementById("down-voice");
 const arrowLeft = document.getElementById("left-voice");
 const arrowRight = document.getElementById("right-voice");
+const selectArrowUp = document.getElementById("upKey-voice");
+const selectArrowDown = document.getElementById("downKey-voice");
+const selectArrowLeft = document.getElementById("leftKey-voice");
+const selectArrowRight = document.getElementById("rightKey-voice");
 const upKey = document.getElementById("upKey");
 const downKey = document.getElementById("downKey");
 const leftKey = document.getElementById("leftKey");
@@ -78,10 +82,10 @@ resetButton.addEventListener("click", function () {
 
   if (toggle.checked && inputContainerEyes.classList.contains("hidden")) {
     // Reset voice container input fields
-    allEmpty = checkAndReset(inputContainerVoice);
+    allEmpty = checkAndResetVoice(inputContainerVoice);
   } else if (inputContainerVoice.classList.contains("hidden")) {
     // Reset eyes container input fields
-    allEmpty = checkAndReset(inputContainerEyes);
+    allEmpty = checkAndResetEyes(inputContainerEyes);
   }
 
   if (allEmpty) {
@@ -90,15 +94,31 @@ resetButton.addEventListener("click", function () {
 });
 
 // Function to reset input fields to default states
-const checkAndReset = (container) => {
+const checkAndResetVoice = (container) => {
+  console.log("TEST query1");
   let allEmpty = true;
-  container.querySelectorAll("input").forEach((input) => {
-    if (input.value !== "") {
-      allEmpty = false;
-    }
-    input.value = "";
-    input.placeholder = input.dataset.placeholder;
-  });
+
+  allEmpty = false;
+  
+  let arrSelect = container.getElementsByClassName("dropdown-select-voice");
+  let arrInput = container.getElementsByClassName("keyword-input")
+  for(var i = 0; i < 4; i++){
+    arrSelect[i].selectedIndex = 0;
+    arrInput[i].value = "";
+  }
+  return allEmpty;
+};
+
+const checkAndResetEyes= (container) => {
+  console.log("TEST query1");
+  let allEmpty = true;
+
+  allEmpty = false;
+  
+  let arrSelect = container.getElementsByClassName("dropdown-select");
+  for(var i = 0; i < 4; i++){
+    arrSelect[i].selectedIndex = 0;
+  }
   return allEmpty;
 };
 
@@ -117,7 +137,7 @@ helpButton.addEventListener("click", function () {
     mainContainer.classList.remove("hidden");
     pauseButton.classList.remove("hidden");
     console.log("TEST");
-    resetButton.classList.remove("hidden")
+    resetButton.classList.remove("hidden");
   }
 });
 
@@ -145,21 +165,40 @@ saveButton.addEventListener("click", function () {
 
 const saveKeysVoice = (container) => {
   let empty = false;
-  container.querySelectorAll("input").forEach((input) => {
-    if (input.value === "") {
+  
+  let inputArray = container.getElementsByClassName("keyword-input");
+  let selectArray = container.getElementsByClassName("dropdown-select-voice");
+
+  for(var i=0; i<4; i++){
+    if((!inputArray[0].value == "" && selectArray[0].selectedIndex == 0) || (inputArray[0].value == "" && !selectArray[0].selectedIndex == 0)){
+      empty = true;
+    }else if((!inputArray[1].value == "" && selectArray[1].selectedIndex == 0) || (inputArray[1].value == "" && !selectArray[1].selectedIndex == 0)){
+      empty = true;
+    }else if((!inputArray[2].value == "" && selectArray[2].selectedIndex == 0) || (inputArray[2].value == "" && !selectArray[2].selectedIndex == 0)){
+      empty = true;
+    }else if((!inputArray[3].value == "" && selectArray[3].selectedIndex == 0) || (inputArray[3].value == "" && !selectArray[3].selectedIndex == 0)){
       empty = true;
     }
-  });
-  if (empty) {
-    alert("Some Keywords missing!!");
-  } else {
+  }
+
+  if(empty){
+    alert("You must choose a keyword and a keybind")
+  }
+    const selectArrowUpValue = selectArrowUp.selectedIndex == 0 ? "" : selectArrowUp.value
+    const selectArrowDownValue = selectArrowDown.selectedIndex == 0 ? "" : selectArrowDown.value
+    const selectArrowLeftValue = selectArrowLeft.selectedIndex == 0 ? "" : selectArrowLeft.value
+    const selectArrowRightValue = selectArrowRight.selectedIndex == 0 ? "" : selectArrowRight.value
     const arrowUpInput = arrowUp.value;
     const arrowDownInput = arrowDown.value;
     const arrowLeftInput = arrowLeft.value;
     const arrowRightInput = arrowRight.value;
-
+ 
     chrome.storage.sync.set(
       {
+        selectArrowUp: selectArrowUpValue,
+        selectArrowDown: selectArrowDownValue,
+        selectArrowLeft: selectArrowLeftValue,
+        selectArrowRight: selectArrowRightValue,
         arrowUp: arrowUpInput,
         arrowDown: arrowDownInput,
         arrowLeft: arrowLeftInput,
@@ -169,21 +208,13 @@ const saveKeysVoice = (container) => {
         console.log("Settings saved");
       }
     );
-  }
 };
 
 const saveKeysEyes = (container) => {
-  let empty = false;
-  container.querySelectorAll("input").forEach((input) => {
-    if (input.value === "") {
-      empty = true;
-    }
-  });
-
-  const upKeyInput = upKey.value;
-  const downKeyInput = downKey.value;
-  const leftKeyInput = leftKey.value;
-  const rightKeyInput = rightKey.value;
+  const upKeyInput = upKey.selectedIndex == 0 ? "" : upKey.value;
+  const downKeyInput = downKey.selectedIndex == 0 ? "" : downKey.value;
+  const leftKeyInput = leftKey.selectedIndex == 0 ? "" : leftKey.value;
+  const rightKeyInput = rightKey.selectedIndex == 0 ? "" : rightKey.value;
 
   chrome.storage.sync.set(
     {
@@ -200,12 +231,16 @@ const saveKeysEyes = (container) => {
 
 const restoreVoiceOptions = () => {
   chrome.storage.sync.get(
-    ["arrowUp", "arrowDown", "arrowLeft", "arrowRight"],
+    ["arrowUp", "arrowDown", "arrowLeft", "arrowRight", "selectArrowUp", "selectArrowDown", "selectArrowLeft", "selectArrowRight"],
     function (result) {
-      arrowUp.value = result.arrowUp;
-      arrowDown.value = result.arrowDown;
-      arrowLeft.value = result.arrowLeft;
-      arrowRight.value = result.arrowRight;
+      result.arrowUp == undefined ? arrowUp.value = "" : arrowUp.value = result.arrowUp;
+      result.arrowDown == undefined ? arrowDown.value = "" : arrowDown.value = result.arrowDown;
+      result.arrowLeft == undefined ? arrowLeft.value = "" : arrowLeft.value = result.arrowLeft;
+      result.arrowRight == undefined ? arrowRight.value = "" : arrowRight.value = result.arrowRight;
+      result.selectArrowUp == undefined || result.selectArrowUp == "" ? selectArrowUp.selectedIndex = 0 : selectArrowUp.value = result.selectArrowUp;
+      result.selectArrowDown == undefined || result.selectArrowDown == "" ? selectArrowDown.selectedIndex = 0 : selectArrowDown.value = result.selectArrowDown;
+      result.selectArrowLeft == undefined || result.selectArrowLeft == "" ? selectArrowLeft.selectedIndex = 0 : selectArrowLeft.value = result.selectArrowLeft;
+      result.selectArrowRight == undefined || result.selectArrowRight == "" ? selectArrowRight.selectedIndex = 0 : selectArrowRight.value = result.selectArrowRight;;
     }
   );
 };
@@ -214,14 +249,10 @@ const restoreEyeOptions = () => {
   chrome.storage.sync.get(
     ["upKey", "downKey", "leftKey", "rightKey"],
     function (result) {
-      if (result.upKey === undefined) {
-        upKey.value = "";
-      } else {
-        upKey.value = result.upKey;
-      }
-      downKey.value = result.downKey;
-      leftKey.value = result.leftKey;
-      rightKey.value = result.rightKey;
+      result.upKey == undefined || result.upKey == "" ? upKey.selectedIndex = 0 : upKey.value = result.upKey; 
+      result.downKey == undefined || result.downKey == "" ? downKey.selectedIndex = 0 : downKey.value = result.downKey;
+      result.leftKey == undefined || result.leftKey == "" ? leftKey.selectedIndex = 0 : leftKey.value = result.leftKey;
+      result.rightKey == undefined || result.rightKey == "" ? rightKey.selectedIndex = 0 : rightKey.value = result.rightKey;
     }
   );
 };
@@ -254,7 +285,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-
   // init visibility of grid buttons
   gridOnButton.classList.add("hidden");
   gridOffButton.classList.remove("hidden");
